2aec62a765fc447ce2f887428f1191e7567f7ad5
athulrajts <athulrajts@gmail.com>
2026-08-23 03:31:57 +0530

    cleanup template selector

--- TotoroNext.Module/Controls/DataContainerEditor.axaml.cs
+++ TotoroNext.Module/Controls/DataContainerEditor.axaml.cs
@@ -1,7 +1,5 @@
-﻿using System.Diagnostics;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Templates;
 
 namespace TotoroNext.Module.Controls;
 
@@ -20,33 +18,4 @@ public partial class DataContainerEditor : UserControl
         get => GetValue(OptionsProperty);
         set => SetValue(OptionsProperty, value);
     }
-}
-
-public class DataContainerPropertyTemplateSelector : IDataTemplate
-{
-    public IDataTemplate? TextTemplate { get; set; }
-    public IDataTemplate? ComboBoxTemplate { get; set; }
-    public IDataTemplate? ToggleSwitchTemplate { get; set; }
-    public IDataTemplate? FileBrowserTemplate { get; set; }
-    public IDataTemplate? NumberBoxTemplate { get; set; }
-    
-    public Control? Build(object? param)
-    {
-        if (param is not DataContainerProperty item)
-        {
-            return null;
-        }
-
-        return item.EditorType switch
-        {
-            SpecialEditorType.TextBox => TextTemplate?.Build(param),
-            SpecialEditorType.ComboBox => ComboBoxTemplate?.Build(param),
-            SpecialEditorType.ToggleSwitch => ToggleSwitchTemplate?.Build(param),
-            SpecialEditorType.FileBrowser => FileBrowserTemplate?.Build(param),
-            SpecialEditorType.NumberBox => NumberBoxTemplate?.Build(param),
-            _ => throw new UnreachableException()
-        };
-    }
-
-    public bool Match(object? data) => data is DataContainerProperty;
 }
---
+++ TotoroNext.Module/Controls/DataContainerPropertyTemplateSelector.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Data;
+using Avalonia.Layout;
+using Avalonia.Markup.Declarative;
+using Avalonia.Media;
+using Ursa.Controls;
+
+namespace TotoroNext.Module.Controls;
+
+public class DataContainerPropertyTemplateSelector : IDataTemplate
+{
+    public Control? Build(object? param)
+    {
+        return param is DataContainerProperty item ? CreateSettingsCard(item) : null;
+    }
+
+    public bool Match(object? data)
+    {
+        return data is DataContainerProperty;
+    }
+
+    private static SettingsCard CreateSettingsCard(DataContainerProperty property)
+    {
+        return new SettingsCard
+        {
+            Header = property.DisplayName ?? "",
+            Description = property.Description ?? "",
+            Editor = CreateEditor(property)
+        };
+    }
+
+    private static Control CreateEditor(DataContainerProperty property)
+    {
+        return property.EditorType switch
+        {
+            SpecialEditorType.TextBox => CreateTextBox(property),
+            SpecialEditorType.ComboBox => CreateComboBox(property),
+            SpecialEditorType.ToggleSwitch => CreateToggleSwitch(property),
+            SpecialEditorType.FileBrowser => CreatePathPicker(property),
+            SpecialEditorType.NumberBox => CreateNumberBox(property),
+            _ => throw new UnreachableException()
+        };
+    }
+
+    private static NumericIntUpDown CreateNumberBox(DataContainerProperty property)
+    {
+        var control = new NumericIntUpDown().MinWidth(100);
+        control.Bind(NumericIntUpDown.ValueProperty, new Binding(nameof(property.Value))
+        {
+            Source = property,
+            Mode = BindingMode.TwoWay
+        });
+        return control;
+    }
+
+    private static PathPicker CreatePathPicker(DataContainerProperty property)
+    {
+        return new PathPicker()
+               .Title("Browse")
+               .AllowMultiple(false)
+               .UsePickerType(UsePickerTypes.OpenFile)
+               .HorizontalAlignment(HorizontalAlignment.Stretch)
+               .MinWidth(300)
+               .SelectedPathsText(property, x => x.Value);
+    }
+
+    private static ToggleSwitch CreateToggleSwitch(DataContainerProperty property)
+    {
+        return new ToggleSwitch().IsChecked(property, x => x.IsChecked);
+    }
+
+    private static ComboBox CreateComboBox(DataContainerProperty property)
+    {
+        return new ComboBox()
+               .Width(250)
+               .HorizontalAlignment(HorizontalAlignment.Stretch)
+               .SelectedItem(property, x => x.Value)
+               .ItemsSource(property, x => x.AllowedValues);
+    }
+
+    private static TextBox CreateTextBox(DataContainerProperty property)
+    {
+        return new TextBox()
+               .MaxWidth(350)
+               .MinWidth(200)
+               .TextWrapping(TextWrapping.WrapWithOverflow)
+               .Text(property, x => x.Value);
+    }
+}
